c2e6a7d2d58a7a39974534726690ed6ed4c033c7
Szymon Wiśniewski <szymwisn@gmail.com>
2020-03-28 22:05:44 +0100

    Working fibonacci

--- lab2/fibonacci.s
+++ lab2/fibonacci.s
@@ -16,12 +16,12 @@ RDONLY = 0
 .globl _start
 _start:
 
-	movq $4, %rax
+	movq $10, %rax
 	pushq %rax
 	call fibonacci
 	addq $8, %rsp
 
-	movl %eax, %ebx
+	movl %ecx, %ebx
 
 	exit:
 		movl $EXIT, %eax
@@ -34,41 +34,41 @@ _start:
 		movq %rsp, %rbp
 		subq $8, %rsp
 
-		movl 16(%rbp), %eax
-		movl %eax, -8(%rbp)
+		movl 16(%rbp), %eax		# pierwszy argument funkcji do eax
+		movl %eax, -8(%rbp)		# local variable
 
-		cmpl $0, -8(%rbp)
+		cmpl $0, -8(%rbp)		# f(0) = 0
 		je return_0
 
-		cmpl $1, -8(%rbp)
+		cmpl $1, -8(%rbp)		# f(1) = 1
 		je return_1
 		
 		movl -8(%rbp), %eax
-		dec %eax
-		pushq %rax
-		call fibonacci
-		addq $8, %rsp		
+		dec %eax			# n - 1
+		movl %eax, %ebx			# zapisanie wartosci n - 1 w ebx
+		pushq %rbx			# zachowanie wartosci n - 1
+		pushq %rax			# zachowanie wartosci n - 1
+		call fibonacci			# rekurencyjne obliczenie f(n - 1)
+		addq $8, %rsp			
 
-		pushq %rax
-
-		movl -8(%rbp), %ebx
-		dec %ebx
-		pushq %rbx
-		call fibonacci
+		popq %rax			# przywrocenie wartosci n - 1
+		dec %eax			# n - 2
+		pushq %rcx			# zachowanie wartosci f(n - 1)
+		pushq %rax			# zachowanie wartosci n - 2
+		call fibonacci			# rekurencyjne obliczenie f(n - 2)
 		addq $8, %rsp
 		
-		movl %eax, %ebx
+		popq %rax			# przywrocenie wartosci f(n - 1)
 
-		popq %rax
-		addl %ebx, %eax	
+		addl %eax, %ecx			# obliczenie f(n - 2) + f(n - 1)
 		jmp end	
 
 		return_0:	
-			movl $0, %eax
+			movl $0, %ecx
 			jmp end
 
 		return_1:
-			movl $1, %eax
+			movl $1, %ecx
 			jmp end
 
 		end:
